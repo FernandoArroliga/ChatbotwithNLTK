8e9b5494dd5c06bb4dc3136c7d5de13e1d63db16
Fernando Arroliga <FernandoArroliga@hotmail.com>
2023-09-26 06:04:57 -0600

    "First training model process"

--- train.py
+++ train.py
@@ -105,4 +105,19 @@ for epoch in range(num_epochs):
     if (epoch +1) % 100 == 0:
         print(f"epoch {epoch+1}/{num_epochs}, loss={loss.item():.4f}")
 
-#print(f"final loss, loss={loss.item():.4f}")
+print(f"final loss, loss={loss.item():.4f}")
+
+# Save Data
+data = {
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "output_size": output_size,
+    "hidden_size": hidden_size,
+    "all_words": all_words,
+    "tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f"training complete, file saved to {FILE}")
