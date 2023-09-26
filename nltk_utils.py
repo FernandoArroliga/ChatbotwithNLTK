1409bb485a345d8e9824fec844438cfd12828091
Fernando Arroliga <FernandoArroliga@hotmail.com>
2023-09-26 00:59:20 -0600

    "Create basic project functions"

---
+++ nltk_utils.py
@@ -0,0 +1,20 @@
+# Importing the useful library
+import nltk
+
+# Download nltk package for make tokenize
+nltk.download("punkt")
+
+# importing the package for make stemming
+from nltk.stem.porter import PorterStemmer
+
+# Create the stemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
