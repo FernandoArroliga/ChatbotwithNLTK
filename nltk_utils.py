7e073bb64ac998422a2a41e003bdb21d1d12c863
Fernando Arroliga <FernandoArroliga@hotmail.com>
2023-09-26 02:22:57 -0600

    "Create bag of words method"

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,6 @@
 # Importing the useful library
 import nltk
+import numpy as np
 
 # Download nltk package for make tokenize
 nltk.download("punkt")
@@ -17,5 +18,13 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    
+    return bag
 
